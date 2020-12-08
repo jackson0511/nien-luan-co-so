e860024f2f05bc58fdb746a327a0251928066ca7
jackson0511 <nguyenduchuy1999@gmail.com>
2020-12-08 15:20:04 +0700

    add admin login

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group(['prefix' => 'admin'], function () {
+
+    Route::get('login', function () {
+        return view('admin.login.login');
+    });
+
     //1.Tour
     Route::get('/tour-list', 'TourController@index')->name('tour-list');
     Route::get('/create-tour', 'TourController@create')->name('create-tour');
